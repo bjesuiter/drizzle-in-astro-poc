fe9ec4462e3d25a1a4380bacdac36dce1597d3f7
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-25 23:31:25 +0200

    add comment on our way of environment parsing

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="astro/client" />
+
+// This is the standard astro + vite way of accessing environment variables
+// however, we use the zod way instead: https://github.com/drizzle-team/drizzle-orm/blob/main/examples/libsql/src/env.ts
+
+// interface ImportMetaEnv {
+//     readonly DB_PASSWORD: string;
+//     readonly PUBLIC_POKEAPI: string;
+//     // mehr Umgebungsvariablen...
+//   }
+
+//   interface ImportMeta {
+//     readonly env: ImportMetaEnv;
+//   }
