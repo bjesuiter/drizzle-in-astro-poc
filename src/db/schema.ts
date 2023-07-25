3f936375dc1e1dfc332ae166cd79a965b0805b63
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-26 01:00:58 +0200

    refine schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,5 @@
-import { sql, type InferModel } from "drizzle-orm";
+import type { InferModel } from "drizzle-orm";
+import { sql } from "drizzle-orm";
 import {
   integer,
   sqliteTable,
@@ -6,24 +7,28 @@ import {
   uniqueIndex,
 } from "drizzle-orm/sqlite-core";
 
-import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
 export const users = sqliteTable("users", {
   id: integer("id").primaryKey(),
-  fullName: text("full_name"),
-  phone: text("phone"),
+  name: text("name"),
+  email: text("email"),
 });
 
 export type User = InferModel<typeof users>; // return type when queried
 export type InsertUser = InferModel<typeof users, "insert">; // insert type
 
-export const posts = sqliteTable('posts', {
-	id: integer('id').primaryKey(),
-	title: text('title').notNull(),
-	body: text('body').notNull(),
-	authorId: integer('author_id').notNull().references(() => users.id),
-	createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
-	updateAt: integer('updated_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
+export const posts = sqliteTable("posts", {
+  id: integer("id").primaryKey(),
+  title: text("title").notNull(),
+  body: text("body").notNull(),
+  authorId: integer("author_id").notNull().references(() => users.id),
+  createdAt: integer("created_at", { mode: "timestamp" }).default(
+    sql`(strftime('%s', 'now'))`,
+  ),
+  updateAt: integer("updated_at", { mode: "timestamp" }).default(
+    sql`(strftime('%s', 'now'))`,
+  ),
 });
 export const insertPostSchema = createInsertSchema(posts);
 export const selectPostSchema = createSelectSchema(posts);
